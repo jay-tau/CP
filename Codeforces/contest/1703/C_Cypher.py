2932ec1c21dd71fcad7ff7270f3ba71d86f59ba9
Joel Tony <joeltony72@gmail.com>
2022-10-11 17:50:20 +0530

    Add CF contest 1703

---
+++ Codeforces/contest/1703/C_Cypher.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    n = int(input())
+    final_sequence = list(map(int, input().split()))
+
+    for i in range(n):
+        temp = input().split()
+        final_number = int(temp[0])
+        moves = temp[1]
+        net_move = 0
+        for move in moves:
+            if move == 'U':
+                net_move += 1
+            else:
+                net_move -= 1
+        print(((final_sequence[i] - net_move)%10), end=' ')
+    print()
