a5ce64656c9f4a1d787a1a5cdcf9dc1e4e28640b
Joel Tony <joeltony72@gmail.com>
2022-10-19 01:44:02 +0530

    Add 492B

---
+++ Codeforces/problemset/492/B_Vanya_and_Lanterns.py
@@ -0,0 +1,17 @@
+n, l = map(int, input().split())
+a = list(map(int, input().split()))
+
+a.sort()
+
+a_min = min(a); a_max = max(a)
+
+if n == 1:
+	print(max(a_min, l - a_max))
+elif min(a) == 0 and max(a) == l:
+	print(max([a[i+1]-a[i] for i in range(n-1)])/2)
+elif min(a) == 0:
+	print(max((l - a_max), max([a[i+1]-a[i] for i in range(n-1)])/2))
+elif max(a) == l:
+	print(max((a_min), max([a[i+1]-a[i] for i in range(n-1)])/2))
+else:
+	print(max(a_min), (l - a_max), max([a[i+1]-a[i] for i in range(n-1)])/2)
