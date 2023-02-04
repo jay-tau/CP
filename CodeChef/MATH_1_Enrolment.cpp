56512a3cdb83aefe956828d50b2c1bc4981ac337
Joel Tony <joeltony72@gmail.com>
2023-02-04 10:52:25 +0530

    MATH_1_Enrolment.cpp: -	int t; -	cin >> t; -	while (t--) { -		int x,y; -		cin >> x >> y; -		int ans{0}; -		if (y > x) { -			ans = (y - x); -		} -		cout << ans << '\n'; -	} -	return 0; +  int t; +  cin >> t; +  while (t--) { +    int x, y; +    cin >> x >> y; +    int ans{0}; +    if (y > x) { +      ans = (y - x); +    } +    cout << ans << '\n'; +  } +  return 0;

--- CodeChef/MATH_1_Enrolment.cpp
+++ CodeChef/MATH_1_Enrolment.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main() {
-	int t;
-	cin >> t;
-	while (t--) {
-		int x,y;
-		cin >> x >> y;
-		int ans{0};
-		if (y > x) {
-			ans = (y - x);
-		}
-		cout << ans << '\n';
-	}
-	return 0;
+  int t;
+  cin >> t;
+  while (t--) {
+    int x, y;
+    cin >> x >> y;
+    int ans{0};
+    if (y > x) {
+      ans = (y - x);
+    }
+    cout << ans << '\n';
+  }
+  return 0;
 }
