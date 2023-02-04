4b07f5d2e682f27580919679ceb2818fa57b3bc5
Joel Tony <joeltony72@gmail.com>
2023-02-04 10:53:08 +0530

    Fixing the testcase
    The testcase was not correct.
    
    The output should be:
    5
    1 6 5
    
    Instead of:
    5
    1 6 5

--- CodeChef/Meet_In_The_Median.py
+++ CodeChef/Meet_In_The_Median.py
@@ -1,11 +1,13 @@
 import statistics
+
 for testcase in range(int(input())):
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
-    median_max = 0; i_max = 0
-    for i in range(n-k+1):
-        median = statistics.median(a[i:(i+k+1)])
+    median_max = 0
+    i_max = 0
+    for i in range(n - k + 1):
+        median = statistics.median(a[i : (i + k + 1)])
         if median > median_max:
             median_max = median
             i_max = i
-    print(' '.join(map(str, a[i_max:(i_max+k+1)])))
+    print(" ".join(map(str, a[i_max : (i_max + k + 1)])))
