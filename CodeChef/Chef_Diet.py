8c3d533c647daace56127eb9efb0c543a3af99ee
Joel Tony <joeltony72@gmail.com>
2023-02-04 10:51:26 +0530

    Fixing the bug
    The bug was caused by the fact that the variable flag was not initialized
    to True.

--- CodeChef/Chef_Diet.py
+++ CodeChef/Chef_Diet.py
@@ -1,12 +1,13 @@
 for _ in range(int(input())):
-    flag  = True
+    flag = True
     n, k = map(int, input().split())
     a = list(map(int, input().split()))
     current = 0
     for day in range(len(a)):
         current += a[day]
         if k > current:
-            print("NO", (day+1)); flag = False
+            print("NO", (day + 1))
+            flag = False
             break
         current -= k
     if flag:
