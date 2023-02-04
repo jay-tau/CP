acd0e341b96c37b845847d8090a18c595451c13b
Joel Tony <joeltony72@gmail.com>
2023-02-04 10:51:36 +0530

    Fixing the code
    The code was not working as expected.

--- CodeChef/Counting_Words.cpp
+++ CodeChef/Counting_Words.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-	int t;
-    cin >> t;
-    while (t--) {
-        int x,y;
-        cin >> x >> y;
-        cout << (x*y) << '\n';
-    }
-	return 0;
+  int t;
+  cin >> t;
+  while (t--) {
+    int x, y;
+    cin >> x >> y;
+    cout << (x * y) << '\n';
+  }
+  return 0;
 }
