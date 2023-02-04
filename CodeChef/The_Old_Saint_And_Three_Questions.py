23d70008889c606c05e4ee84278f59e39b92c2f0
Joel Tony <joeltony72@gmail.com>
2023-02-04 11:04:33 +0530

    Fixing the formatting of the code
    The code was not formatted properly.

--- CodeChef/The_Old_Saint_And_Three_Questions.py
+++ CodeChef/The_Old_Saint_And_Three_Questions.py
@@ -1,7 +1,7 @@
 for testcase in range(int(input())):
     a = input().split()
     b = input().split()
-    if (a.count('1') == b.count('1')):
+    if a.count("1") == b.count("1"):
         print("Pass")
     else:
         print("Fail")
--- CodeChef/The_Timber_Auction.cpp
+++ CodeChef/The_Timber_Auction.cpp
@@ -2,44 +2,45 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    int m, n;
-    cin >> m >> n;
-    int a[m+1][n+1];
-    long long int pf[m+1][n+1];
-    for (int i = 0; i <= m; i++) {
-        a[i][0] = 0;
-        pf[i][0] = 0;
-    }
-    for (int j = 0; j <= n; j++) {
-        a[0][j] = 0;
-        pf[0][j] = 0;
-    }
+  int m, n;
+  cin >> m >> n;
+  int a[m + 1][n + 1];
+  long long int pf[m + 1][n + 1];
+  for (int i = 0; i <= m; i++) {
+    a[i][0] = 0;
+    pf[i][0] = 0;
+  }
+  for (int j = 0; j <= n; j++) {
+    a[0][j] = 0;
+    pf[0][j] = 0;
+  }
 
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            cin >> a[i][j];
-            pf[i][j] = a[i][j] + pf[i-1][j] + pf[i][j-1] - pf[i-1][j-1];
-        }
+  for (int i = 1; i <= m; i++) {
+    for (int j = 1; j <= n; j++) {
+      cin >> a[i][j];
+      pf[i][j] = a[i][j] + pf[i - 1][j] + pf[i][j - 1] - pf[i - 1][j - 1];
     }
-    /**
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
-            cout << pf[i][j] << " ";
-        }
-        cout << endl;
-    }
-    */
+  }
+  /**
+  for (int i = 0; i <= n; i++) {
+      for (int j = 0; j <= n; j++) {
+          cout << pf[i][j] << " ";
+      }
+      cout << endl;
+  }
+  */
 
-    int q;
-    cin >> q;
-    while (q--) {
-        int y_1, x_1, y_2, x_2;
-        cin >> y_1 >> x_1 >> y_2 >> x_2;
-        long long int ans = pf[y_2][x_2] - pf[y_1-1][x_2] - pf[y_2][x_1-1] + pf[y_1-1][x_1-1];
-        cout << ans << '\n';
-    }
-    return 0;
+  int q;
+  cin >> q;
+  while (q--) {
+    int y_1, x_1, y_2, x_2;
+    cin >> y_1 >> x_1 >> y_2 >> x_2;
+    long long int ans = pf[y_2][x_2] - pf[y_1 - 1][x_2] - pf[y_2][x_1 - 1] +
+                        pf[y_1 - 1][x_1 - 1];
+    cout << ans << '\n';
+  }
+  return 0;
 }
