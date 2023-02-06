ca646a06060b54cc06bf353ab16fc5c2821408bf
Joel Tony <joeltony72@gmail.com>
2023-02-06 19:46:18 +0530

    Fix typos
    - Fix typos in the code
    - Fix typos in the commit message

--- Codeforces/contest/1741/B_Funny_Permutation.py
+++ Codeforces/contest/1741/B_Funny_Permutation.py
@@ -1,11 +1,11 @@
 for _ in range(int(input())):
-	n = int(input())
-	if n == 3:
-		print(-1)
-		continue
-	elif n == 2:
-		print('2 1')
-	elif n % 2 == 0:
-		print((n - 1), n, *range(n-2, 0, -1))
-	else:
-		print(n, (n - 1), *range(1, n-1))
+    n = int(input())
+    if n == 3:
+        print(-1)
+        continue
+    elif n == 2:
+        print("2 1")
+    elif n % 2 == 0:
+        print((n - 1), n, *range(n - 2, 0, -1))
+    else:
+        print(n, (n - 1), *range(1, n - 1))
