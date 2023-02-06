069cefed92202d61d941d4b995b63f6374cf6b22
Joel Tony <joeltony72@gmail.com>
2023-02-06 19:38:45 +0530

    Fix the bug
    The bug was caused by the fact that the code was not using the correct
    algorithm to solve the problem.

--- Codeforces/contest/1705/A_Mark_the_Photographer.cpp
+++ Codeforces/contest/1705/A_Mark_the_Photographer.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, x;
-        cin >> n >> x;
-        int h[2*n];
-        for (int i = 0; i < 2*n; i++) {
-            cin >> h[i];
-        }
-        sort(h, h+(2*n));
-        bool flag = true;
-        for (int i = 0; i < n; i++) {
-            if (!((h[n+i] - h[i]) >= x)) {
-                flag = false;
-                break;
-            }
-        }
-        if (flag) {
-            cout << "YES" << '\n';
-        } else {
-            cout << "NO" << '\n';
-        }
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, x;
+    cin >> n >> x;
+    int h[2 * n];
+    for (int i = 0; i < 2 * n; i++) {
+      cin >> h[i];
     }
+    sort(h, h + (2 * n));
+    bool flag = true;
+    for (int i = 0; i < n; i++) {
+      if (!((h[n + i] - h[i]) >= x)) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      cout << "YES" << '\n';
+    } else {
+      cout << "NO" << '\n';
+    }
+  }
 }
