efb7d1d06e476a8681398933ebb3fe52a1ebce88
Joel Tony <joeltony72@gmail.com>
2023-02-06 11:42:57 +0530

    Fixing the formatting of the code
    The code was not formatted properly.
    
    This commit fixes the formatting of the code.

--- Codeforces/contest/1669/F_Eating_Candies.cpp
+++ Codeforces/contest/1669/F_Eating_Candies.cpp
@@ -2,35 +2,38 @@
 using namespace std;
 
 int main() {
-    int _;
-    cin >> _;
-    while (_--) {
-        int n;
-        cin >> n;
+  int _;
+  cin >> _;
+  while (_--) {
+    int n;
+    cin >> n;
 
-        int l[n];
-        for (int i = 0; i < n; i++) {
-            cin >> l[i];
-        }
+    int l[n];
+    for (int i = 0; i < n; i++) {
+      cin >> l[i];
+    }
 
-        int count = 0;
-        int front_index = 0, back_index = (n - 1);
-        int front_pointer = l[front_index], back_pointer = l[back_index];
+    int count = 0;
+    int front_index = 0, back_index = (n - 1);
+    int front_pointer = l[front_index], back_pointer = l[back_index];
 
-        while (front_index < back_index) {
-            if (front_pointer == back_pointer) {
-                count = (front_index + 1); count += (n - back_index);
-                front_index++; back_index--;
-                front_pointer += l[front_index]; back_pointer += l[back_index];
-            } else if (front_pointer < back_pointer) {
-                front_index++;
-                front_pointer += l[front_index];
-            } else if (front_pointer > back_pointer) {
-                back_index--;
-                back_pointer += l[back_index];
-            }
-        } //end while
-        cout << count << '\n';
-    } // end testcases
-    return 0;
+    while (front_index < back_index) {
+      if (front_pointer == back_pointer) {
+        count = (front_index + 1);
+        count += (n - back_index);
+        front_index++;
+        back_index--;
+        front_pointer += l[front_index];
+        back_pointer += l[back_index];
+      } else if (front_pointer < back_pointer) {
+        front_index++;
+        front_pointer += l[front_index];
+      } else if (front_pointer > back_pointer) {
+        back_index--;
+        back_pointer += l[back_index];
+      }
+    } // end while
+    cout << count << '\n';
+  } // end testcases
+  return 0;
 }
