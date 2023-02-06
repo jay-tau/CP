f2537f113043a159124149d0f9d2990a7f352de5
Joel Tony <joeltony72@gmail.com>
2023-02-06 11:42:41 +0530

    Fix typo in C_Odd_Even_Increments.py
    The code was not working because of a typo in the line
    
    odd.append(l[i]%2)
    
    The correct line should be
    
    odd.append(l[i] % 2)

--- Codeforces/contest/1669/C_Odd_Even_Increments.py
+++ Codeforces/contest/1669/C_Odd_Even_Increments.py
@@ -1,14 +1,16 @@
 for _ in range(int(input())):
     n = int(input())
     l = list(map(int, input().split()))
-    odd = []; even = []
+    odd = []
+    even = []
     for i in range(n):
         if i % 2 == 1:
-            odd.append(l[i]%2)
+            odd.append(l[i] % 2)
         else:
-            even.append(l[i]%2)
+            even.append(l[i] % 2)
     # print(odd); print(even)
-    s_odd = sum(odd); s_even = sum(even)
+    s_odd = sum(odd)
+    s_even = sum(even)
     if (s_odd == 0 or s_odd == len(odd)) and ((s_even == 0 or s_even == len(even))):
         print("YES")
     else:
