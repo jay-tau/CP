7a52187ea249e1ae606e8170b33eb26b6960a613
Joel Tony <joeltony72@gmail.com>
2023-02-06 19:49:10 +0530

    Fixing the test cases
    The test cases were not being read properly.
    
    The problem was that the test cases were being read as a single line.
    
    The solution was to add a newline character at the end of the test cases.

--- Codeforces/contest/1783/A_Make_it_Beautiful.cpp
+++ Codeforces/contest/1783/A_Make_it_Beautiful.cpp
@@ -1,6 +1,6 @@
 /**
  *    author:  jaytau
-**/
+ **/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,7 +8,7 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
 typedef pair<ll, ll> pll;
 
 #define endl "\n"
@@ -21,116 +21,114 @@ typedef pair<ll, ll> pll;
 #define pb push_back
 #define mp make_pair
 
-#define fo(i, n) for (int (i) = 0; (i) < (n); ++(i))
-#define fo1(i, n) for (int (i) = 1; (i) <= (n); ++(i))
-#define foll(i, n) for (ll (i) = 0; (i) < (n); ++(i))
-#define fo1ll(i, n) for (ll (i) = 1; (i) <= (n); ++(i))
+#define fo(i, n) for (int(i) = 0; (i) < (n); ++(i))
+#define fo1(i, n) for (int(i) = 1; (i) <= (n); ++(i))
+#define foll(i, n) for (ll(i) = 0; (i) < (n); ++(i))
+#define fo1ll(i, n) for (ll(i) = 1; (i) <= (n); ++(i))
 
-ll modpower(ll x, ll y, ll p){
-    ll res = 1;x = x % p;
-    while (y > 0){
-        if (y & 1)res = (res * x) % p;
-        y = y >> 1;x = (x * x) % p;
-    }
-    return (res + p) % p;
+ll modpower(ll x, ll y, ll p) {
+  ll res = 1;
+  x = x % p;
+  while (y > 0) {
+    if (y & 1)
+      res = (res * x) % p;
+    y = y >> 1;
+    x = (x * x) % p;
+  }
+  return (res + p) % p;
 }
-ll modinv(ll x, ll p){ return modpower(x, p - 2, p); }
-ll modmul(ll a, ll b, ll p){ return (a%p * b%p)%p; }
-ll modadd(ll a, ll b, ll p){ return (a%p + b%p)%p; }
-ll modsub(ll a, ll b, ll p){ return (a%p - b%p+p)%p; }
-ll moddiv(ll a, ll b, ll p){ return modmul(a, modinv(b, p), p);}
+ll modinv(ll x, ll p) { return modpower(x, p - 2, p); }
+ll modmul(ll a, ll b, ll p) { return (a % p * b % p) % p; }
+ll modadd(ll a, ll b, ll p) { return (a % p + b % p) % p; }
+ll modsub(ll a, ll b, ll p) { return (a % p - b % p + p) % p; }
+ll moddiv(ll a, ll b, ll p) { return modmul(a, modinv(b, p), p); }
 ll factmod(ll n, ll p) {
-    vector<ll> f(p);
-    f[0] = 1;
-    for (ll i = 1; i < p; i++)
-        f[i] = f[i-1] * i % p;
+  vector<ll> f(p);
+  f[0] = 1;
+  for (ll i = 1; i < p; i++)
+    f[i] = f[i - 1] * i % p;
 
-    ll res = 1;
-    while (n > 1) {
-        if ((n/p) % 2)
-            res = p - res;
-        res = res * f[n%p] % p;
-        n /= p;
-    }
-    return res;
+  ll res = 1;
+  while (n > 1) {
+    if ((n / p) % 2)
+      res = p - res;
+    res = res * f[n % p] % p;
+    n /= p;
+  }
+  return res;
 }
 
 template <typename T1, typename T2> // cin >> pair<T1, T2>
 istream &operator>>(istream &istream, pair<T1, T2> &p) {
-    return (istream >> p.first >> p.second);
+  return (istream >> p.first >> p.second);
 }
 
 template <typename T> // cin >> vector<T>
 istream &operator>>(istream &istream, vector<T> &v) {
-    for (auto &it : v)
-        cin >> it;
-    return istream;
+  for (auto &it : v)
+    cin >> it;
+  return istream;
 }
 
 template <typename T1, typename T2> // cout << pair<T1, T2>
 ostream &operator<<(ostream &ostream, const pair<T1, T2> &p) {
-    return (ostream << p.first << " " << p.second);
+  return (ostream << p.first << " " << p.second);
 }
 template <typename T> // cout << vector<T>
 ostream &operator<<(ostream &ostream, const vector<T> &c) {
-    for (auto &it : c)
-        cout << it << " ";
-    return ostream;
+  for (auto &it : c)
+    cout << it << " ";
+  return ostream;
 }
 
-#define fastio()                  \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0)
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0)
 
-#define test() \
-    int _;     \
-    cin >> _;  \
-    while (_--)
+#define test()                                                                 \
+  int _;                                                                       \
+  cin >> _;                                                                    \
+  while (_--)
 
-#define test1() \
-    int _;      \
-    _ = 1;      \
-    while (_--)
-
+#define test1()                                                                \
+  int _;                                                                       \
+  _ = 1;                                                                       \
+  while (_--)
 
 void solve() {
-    int n;
-    cin >> n;
-    vi a(n);
-    cin >> a;
-    sort(all(a));
-    vi beauty(n);
-    int l_ptr = 0, r_ptr = n - 1;
-    fo(i, n) {
-        if (i % 2 == 0) {
-            beauty[i] = a[r_ptr];
-            r_ptr--;
-        } else {
-            beauty[i] = a[l_ptr];
-            l_ptr++;
-        }
+  int n;
+  cin >> n;
+  vi a(n);
+  cin >> a;
+  sort(all(a));
+  vi beauty(n);
+  int l_ptr = 0, r_ptr = n - 1;
+  fo(i, n) {
+    if (i % 2 == 0) {
+      beauty[i] = a[r_ptr];
+      r_ptr--;
+    } else {
+      beauty[i] = a[l_ptr];
+      l_ptr++;
     }
-    // cout << beauty << endl;
-    vi pf(n);
-    pf[0] = beauty[0];
-    fo1(i, n - 1) {
-        pf[i] = pf[i - 1] + beauty[i];
+  }
+  // cout << beauty << endl;
+  vi pf(n);
+  pf[0] = beauty[0];
+  fo1(i, n - 1) { pf[i] = pf[i - 1] + beauty[i]; }
+  for (int i = 1; i < n; i++) {
+    if (pf[i - 1] == beauty[i]) {
+      NO;
+      return;
     }
-    for (int i = 1; i < n; i++) {
-        if (pf[i-1] == beauty[i]) {
-            NO;
-            return;
-        }
-    }
-    YES;
-    cout << beauty << endl;
+  }
+  YES;
+  cout << beauty << endl;
 }
 
 int main() {
-    fastio();
-    test() {
-        solve();
-    }
-    return 0;
+  fastio();
+  test() { solve(); }
+  return 0;
 }
