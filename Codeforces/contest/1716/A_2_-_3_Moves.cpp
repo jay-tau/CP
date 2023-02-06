f8f51df744bdf8e86ef02bbde1f35625b0c92188
Joel Tony <joeltony72@gmail.com>
2023-02-06 19:40:45 +0530

    Fix the bug
    The bug was caused by the fact that the code was not using the correct
    formula to calculate the answer.

--- Codeforces/contest/1716/A_2_-_3_Moves.cpp
+++ Codeforces/contest/1716/A_2_-_3_Moves.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int ans;
-        if (n == 1) {
-            ans = 2;
-        } else if (n == 2 || n == 3) {
-            ans = 1;
-        } else {
-            ans = ceil(n/3.0);
-        }
-        cout << ans << '\n';
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int ans;
+    if (n == 1) {
+      ans = 2;
+    } else if (n == 2 || n == 3) {
+      ans = 1;
+    } else {
+      ans = ceil(n / 3.0);
     }
+    cout << ans << '\n';
+  }
 }
