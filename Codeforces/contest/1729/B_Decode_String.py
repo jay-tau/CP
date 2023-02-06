d33d28fef03c48ab20a54c3d854256521a9c117f
Joel Tony <joeltony72@gmail.com>
2023-02-06 19:44:53 +0530

    Fixing indentation and spacing in B_Decode_String.py and C_Jumping_on_Tiles.cpp
    The files were not properly indented and spaced.
    
    This commit fixes the indentation and spacing in the files.

--- Codeforces/contest/1729/B_Decode_String.py
+++ Codeforces/contest/1729/B_Decode_String.py
@@ -1,19 +1,21 @@
 for _ in range(int(input())):
-	n = int(input())
-	s = input()
-	reversed_ans = ''
-	i = n-1
-	while (i >= 0):
-	# for i in range(n-1, -1, -1):
-		# print(i)
-		element = s[i]
-		if element != '0':
-			reversed_ans += chr(int(element) + 96)
-			i -= 1
-			# print(reversed_ans)
-		else:
-			# Read 3 characters
-			substring = s[(i-2):(i+1)]
-			reversed_ans += chr(int(substring[:-1]) + 96)	# Removes trailing zero from xx0, and returns letter at xx in alphabets
-			i -= 3 # Skips to position after xx0
-	print(reversed_ans[::-1])
+    n = int(input())
+    s = input()
+    reversed_ans = ""
+    i = n - 1
+    while i >= 0:
+        # for i in range(n-1, -1, -1):
+        # print(i)
+        element = s[i]
+        if element != "0":
+            reversed_ans += chr(int(element) + 96)
+            i -= 1
+            # print(reversed_ans)
+        else:
+            # Read 3 characters
+            substring = s[(i - 2) : (i + 1)]
+            reversed_ans += chr(
+                int(substring[:-1]) + 96
+            )  # Removes trailing zero from xx0, and returns letter at xx in alphabets
+            i -= 3  # Skips to position after xx0
+    print(reversed_ans[::-1])
