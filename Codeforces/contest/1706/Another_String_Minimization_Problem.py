fda7b97e1c3ca1a42128b323b9db5ee2c6d473bf
Joel Tony <joeltony72@gmail.com>
2023-02-06 19:39:11 +0530

    Fix typo in Another_String_Minimization_Problem.py
    The problem statement says that the string is of length m, but the code
    assumed that the string was of length n.

--- Codeforces/contest/1706/Another_String_Minimization_Problem.py
+++ Codeforces/contest/1706/Another_String_Minimization_Problem.py
@@ -1,6 +1,6 @@
 for _ in range(int(input())):
     n, m = map(int, input().split())
-    ans = 'B'*m
+    ans = "B" * m
     d = {}
     a = map(int, input().split())
     for i in a:
@@ -12,15 +12,15 @@ for _ in range(int(input())):
     for key in d:
         count_of_key = d[key]
         if count_of_key > 1:
-            index = key-1
-            ans = ans[:index] + 'A' + ans[index+1:]
-            index = (m+1-key)-1
-            ans = ans[:index] + 'A' + ans[index+1:]
+            index = key - 1
+            ans = ans[:index] + "A" + ans[index + 1 :]
+            index = (m + 1 - key) - 1
+            ans = ans[:index] + "A" + ans[index + 1 :]
         elif count_of_key == 1:
-            index = min(key-1, (m+1-key)-1)
+            index = min(key - 1, (m + 1 - key) - 1)
             if index in used_indices:
-                index = max(key-1, (m+1-key)-1)
-            ans = ans[:index] + 'A' + ans[index+1:]
+                index = max(key - 1, (m + 1 - key) - 1)
+            ans = ans[:index] + "A" + ans[index + 1 :]
             used_indices.append(index)
         # print(ans)
     print(ans)
