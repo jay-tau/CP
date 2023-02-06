97d4699fa62d08f0629151910e6b177ef781448b
Joel Tony <joeltony72@gmail.com>
2023-02-06 19:45:12 +0530

    Fixing the indentation of the code
    The code was not properly indented.

--- Codeforces/contest/1729/E_Guess_the_Cycle_Size.cpp
+++ Codeforces/contest/1729/E_Guess_the_Cycle_Size.cpp
@@ -1,6 +1,6 @@
 /**
  *    author:  jaytau
-**/
+ **/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,7 +8,7 @@ using namespace std;
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
 
 #define endl "\n"
 #define YES cout << "YES" << endl
@@ -20,90 +20,92 @@ typedef pair<int,int> pi;
 #define pb push_back
 #define mp make_pair
 
-#define fo(i, n) for (int (i) = 0; (i) < (n); (i)++)
-#define fo1(i, n) for (int (i) = 1; (i) <= (n); (i)++)
-#define foll(i, n) for (ll (i) = 0; (i) < (n); (i)++)
-#define fo1ll(i, n) for (ll (i) = 1; (i) <= (n); (i)++)
+#define fo(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define fo1(i, n) for (int(i) = 1; (i) <= (n); (i)++)
+#define foll(i, n) for (ll(i) = 0; (i) < (n); (i)++)
+#define fo1ll(i, n) for (ll(i) = 1; (i) <= (n); (i)++)
 
-ll modpower(ll x, ll y, ll p){
-    ll res = 1;x = x % p;
-    while (y > 0){
-        if (y & 1)res = (res * x) % p;
-        y = y >> 1;x = (x * x) % p;
-    }
-    return (res + p) % p;
+ll modpower(ll x, ll y, ll p) {
+  ll res = 1;
+  x = x % p;
+  while (y > 0) {
+    if (y & 1)
+      res = (res * x) % p;
+    y = y >> 1;
+    x = (x * x) % p;
+  }
+  return (res + p) % p;
 }
-ll modinv(ll x, ll p){return modpower(x, p - 2, p);}
-ll modmul(ll a,ll b, ll p){return (a%p*b%p)%p;}
-ll modadd(ll a,ll b, ll p){return (a%p+b%p)%p;}
-ll modsub(ll a,ll b, ll p){return (a%p-b%p+p)%p;}
-ll moddiv(ll a,ll b, ll p){return modmul(a,modinv(b, p), p);}
+ll modinv(ll x, ll p) { return modpower(x, p - 2, p); }
+ll modmul(ll a, ll b, ll p) { return (a % p * b % p) % p; }
+ll modadd(ll a, ll b, ll p) { return (a % p + b % p) % p; }
+ll modsub(ll a, ll b, ll p) { return (a % p - b % p + p) % p; }
+ll moddiv(ll a, ll b, ll p) { return modmul(a, modinv(b, p), p); }
 ll factmod(ll n, ll p) {
-    vector<ll> f(p);
-    f[0] = 1;
-    for (ll i = 1; i < p; i++)
-        f[i] = f[i-1] * i % p;
+  vector<ll> f(p);
+  f[0] = 1;
+  for (ll i = 1; i < p; i++)
+    f[i] = f[i - 1] * i % p;
 
-    ll res = 1;
-    while (n > 1) {
-        if ((n/p) % 2)
-            res = p - res;
-        res = res * f[n%p] % p;
-        n /= p;
-    }
-    return res;
+  ll res = 1;
+  while (n > 1) {
+    if ((n / p) % 2)
+      res = p - res;
+    res = res * f[n % p] % p;
+    n /= p;
+  }
+  return res;
 }
 
-#define fastio()                  \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);                   \
-	cout.tie(0)
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0)
 
-#define test() \
-	int _;     \
-	cin >> _;  \
-	while (_--)
+#define test()                                                                 \
+  int _;                                                                       \
+  cin >> _;                                                                    \
+  while (_--)
 
-#define test1() \
-	int t;      \
-	t = 1;      \
-	while (t--)
+#define test1()                                                                \
+  int t;                                                                       \
+  t = 1;                                                                       \
+  while (t--)
 
-
-void swap(int* num1, int* num2) {
-	int temp;
-	temp = *num1;
-	*num1 = *num2;
-	*num2=temp;
+void swap(int *num1, int *num2) {
+  int temp;
+  temp = *num1;
+  *num1 = *num2;
+  *num2 = temp;
 }
 
 void solve() {
-	ll n;
-	ll a = 1, b = 2;
+  ll n;
+  ll a = 1, b = 2;
 
-	while (true) {
-		// `? 1 b` and `? b 1`
-		cout << "? " << a << " " << b << endl << flush;
-		ll previous_query; 	cin >> previous_query;
-		cout << "? " << b << " " << a << endl << flush;
-		ll current_query;		cin >> current_query;
+  while (true) {
+    // `? 1 b` and `? b 1`
+    cout << "? " << a << " " << b << endl << flush;
+    ll previous_query;
+    cin >> previous_query;
+    cout << "? " << b << " " << a << endl << flush;
+    ll current_query;
+    cin >> current_query;
 
-		if ((current_query == -1) || (previous_query == -1)) {
-			cout << "! " << (b - 1) << endl << flush;
-			return;
-		} else if (current_query != previous_query) {
-			cout << "! " << (current_query + previous_query) << endl << flush;
-			return;
-		} else {
-			b++;
-		}
-	}
+    if ((current_query == -1) || (previous_query == -1)) {
+      cout << "! " << (b - 1) << endl << flush;
+      return;
+    } else if (current_query != previous_query) {
+      cout << "! " << (current_query + previous_query) << endl << flush;
+      return;
+    } else {
+      b++;
+    }
+  }
 }
 
 int main() {
-	fastio();
-	test1() {
-		solve();
-	}
-	return 0;
+  fastio();
+  test1() { solve(); }
+  return 0;
 }
