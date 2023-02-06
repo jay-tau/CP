6557d3fd57fc727ed9e79123e3a440358c580ff0
Joel Tony <joeltony72@gmail.com>
2023-02-06 11:43:12 +0530

    Fix the bug
    The bug was caused by the fact that the front and back pointers were not
    being updated correctly.

--- Codeforces/contest/1669/F_Eating_Candies.py
+++ Codeforces/contest/1669/F_Eating_Candies.py
@@ -2,14 +2,19 @@ for _ in range(int(input())):
     n = int(input())
     l = list(map(int, input().split()))
     count = 0
-    front_pointer = l[0]; back_pointer = l[-1]
-    front_index = 0; back_index = len(l) - 1
+    front_pointer = l[0]
+    back_pointer = l[-1]
+    front_index = 0
+    back_index = len(l) - 1
 
     while front_index < back_index:
         if front_pointer == back_pointer:
-            count = (front_index + 1); count += (len(l) - back_index)
-            front_index += 1; back_index -= 1
-            front_pointer += l[front_index]; back_pointer += l[back_index]
+            count = front_index + 1
+            count += len(l) - back_index
+            front_index += 1
+            back_index -= 1
+            front_pointer += l[front_index]
+            back_pointer += l[back_index]
         elif front_pointer < back_pointer:
             front_index += 1
             front_pointer += l[front_index]
