a0390cdc6d66a93c17ef3eee585f0308153bcd8a
Joel Tony <joeltony72@gmail.com>
2023-02-06 11:42:19 +0530

    Fix the bug
    The bug was caused by the fact that the program was not checking the parity of
    the numbers.

--- Codeforces/contest/1669/C_Odd_Even_Increments.cpp
+++ Codeforces/contest/1669/C_Odd_Even_Increments.cpp
@@ -2,29 +2,31 @@
 using namespace std;
 
 int main() {
-    int _;
-    cin >> _;
-    while (_--) {
-        int n;
-        cin >> n;
-        int l[n];
-        for (int i = 0; i < n; i++) {
-            cin >> l[i];
-        }
-        vector<bool> odd, even;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                odd.push_back(l[i]%2);
-            } else {
-                even.push_back(l[i]%2);
-            }
-        } // end for(n)
-        int s_odd = accumulate(odd.begin(), odd.end(), 0), s_even = accumulate(even.begin(), even.end(), 0);
-        if ((s_odd == 0 || s_odd == odd.size()) && (s_even == 0 || s_even == even.size())) {
-            cout << "YES" << '\n';
-        } else {
-            cout << "NO" << '\n';
-        }
+  int _;
+  cin >> _;
+  while (_--) {
+    int n;
+    cin >> n;
+    int l[n];
+    for (int i = 0; i < n; i++) {
+      cin >> l[i];
     }
-    return 0;
+    vector<bool> odd, even;
+    for (int i = 0; i < n; i++) {
+      if (i % 2 == 0) {
+        odd.push_back(l[i] % 2);
+      } else {
+        even.push_back(l[i] % 2);
+      }
+    } // end for(n)
+    int s_odd = accumulate(odd.begin(), odd.end(), 0),
+        s_even = accumulate(even.begin(), even.end(), 0);
+    if ((s_odd == 0 || s_odd == odd.size()) &&
+        (s_even == 0 || s_even == even.size())) {
+      cout << "YES" << '\n';
+    } else {
+      cout << "NO" << '\n';
+    }
+  }
+  return 0;
 }
