8c2b3b747cbb19ff48ade8108e26b749e34a46e8
Joel Tony <joeltony72@gmail.com>
2023-02-06 11:41:19 +0530

    Fix typo
    The last line was missing a newline character.

--- Codeforces/contest/1669/A_Division.cpp
+++ Codeforces/contest/1669/A_Division.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        if (n >= 1900) {
-            cout << "Division 1";
-        } else if (1600 <= n && n <= 1899) {
-            cout << "Division 2";
-        } else if (1400 <= n && n <= 1599) {
-            cout << "Division 3";
-        } else if (n <= 1399) {
-            cout << "Division 4";
-        }
-        cout << '\n';
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    if (n >= 1900) {
+      cout << "Division 1";
+    } else if (1600 <= n && n <= 1899) {
+      cout << "Division 2";
+    } else if (1400 <= n && n <= 1599) {
+      cout << "Division 3";
+    } else if (n <= 1399) {
+      cout << "Division 4";
     }
-    return 0;
+    cout << '\n';
+  }
+  return 0;
 }
