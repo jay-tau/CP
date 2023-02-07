fdecc708452fe4a080d35fb0bad40e74c1b18111
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:27:14 +0530

    Fix typo in A_Presents.cpp
    The typo was in the for loop condition.

--- Codeforces/problemset/136/A_Presents.cpp
+++ Codeforces/problemset/136/A_Presents.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main() {
-    int n{};
-    cin >> n;
-    int ans[n];
-    for (int i = 0; i < n; i++) {
-        int p_i{};
-        cin >> p_i;
-        ans[p_i-1] = (i+1);
-    }
-    for (int i = 0; i < n; i++) {
-        cout << ans[i] << (i == (n-1) ? '\n' : ' ');
-    }
-    return 0;
+  int n{};
+  cin >> n;
+  int ans[n];
+  for (int i = 0; i < n; i++) {
+    int p_i{};
+    cin >> p_i;
+    ans[p_i - 1] = (i + 1);
+  }
+  for (int i = 0; i < n; i++) {
+    cout << ans[i] << (i == (n - 1) ? '\n' : ' ');
+  }
+  return 0;
 }
