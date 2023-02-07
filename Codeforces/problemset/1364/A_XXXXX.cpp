f34025cc7189df1a00c4e907f6333de4ccd26e5a
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:39:52 +0530

    Fixes #1
    - Fixes the bug where the program does not compile
    - Fixes the bug where the program does not run

--- Codeforces/problemset/1364/A_XXXXX.cpp
+++ Codeforces/problemset/1364/A_XXXXX.cpp
@@ -1,19 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main () {
-    int _;
-    cin >> _;
-    while (_--) {
-        int n, x;
-        cin >> n >> x;
-        int a[n];
-        for (int i = 0; i < n; i++) {
-            int a_i;
-            cin >> a_i;
-            a[i] = (a_i % x);
-        }
-
+int main() {
+  int _;
+  cin >> _;
+  while (_--) {
+    int n, x;
+    cin >> n >> x;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+      int a_i;
+      cin >> a_i;
+      a[i] = (a_i % x);
     }
-    return 0;
+  }
+  return 0;
 }
--- Codeforces/problemset/1366/A_Shovels_and_Swords.cpp
+++ Codeforces/problemset/1366/A_Shovels_and_Swords.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    int _;
-    cin >> _;
-    while (_--) {
-        int a, b;
-        cin >> a >> b;
-        cout << min(((a+b)/3), min(a, b)) << '\n';
-    }
+  int _;
+  cin >> _;
+  while (_--) {
+    int a, b;
+    cin >> a >> b;
+    cout << min(((a + b) / 3), min(a, b)) << '\n';
+  }
 }
