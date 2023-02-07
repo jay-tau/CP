b05503456844c11af64c87118240a6aa375a617f
Joel Tony <joeltony72@gmail.com>
2023-02-07 19:11:58 +0530

    Add a newline at the end of each file
    This is a good practice to follow.

--- Codeforces/problemset/1722/A_Spell_Check.py
+++ Codeforces/problemset/1722/A_Spell_Check.py
@@ -1,25 +1,25 @@
 for _ in range(int(input())):
-	n = int(input())
-	s = input()
-	if n != 5:
-		print("NO")
-		continue
+    n = int(input())
+    s = input()
+    if n != 5:
+        print("NO")
+        continue
 
-	d_s = {}
-	for char_s in s:
-		if char_s in d_s:
-			d_s[char_s] += 1
-		else:
-			d_s[char_s] = 1
+    d_s = {}
+    for char_s in s:
+        if char_s in d_s:
+            d_s[char_s] += 1
+        else:
+            d_s[char_s] = 1
 
-	d_t = {}
-	for char_t in "Timur":
-		if char_t in d_t:
-			d_t[char_t] += 1
-		else:
-			d_t[char_t] = 1
+    d_t = {}
+    for char_t in "Timur":
+        if char_t in d_t:
+            d_t[char_t] += 1
+        else:
+            d_t[char_t] = 1
 
-	if d_s == d_t:
-		print("YES")
-	else:
-		print("NO")
+    if d_s == d_t:
+        print("YES")
+    else:
+        print("NO")
--- Codeforces/problemset/1730/A_Planets.py
+++ Codeforces/problemset/1730/A_Planets.py
@@ -1,10 +1,10 @@
 for _ in range(int(input())):
-	n, c = map(int, input().split())
-	a = list(map(int, input().split()))
-	l = [0 for i in range(1, 101+1)]
-	for a_i in a:
-		l[a_i] += 1
-	cost = 0
-	for l_i in l:
-		cost += min(l_i, c)
-	print(cost)
+    n, c = map(int, input().split())
+    a = list(map(int, input().split()))
+    l = [0 for i in range(1, 101 + 1)]
+    for a_i in a:
+        l[a_i] += 1
+    cost = 0
+    for l_i in l:
+        cost += min(l_i, c)
+    print(cost)
