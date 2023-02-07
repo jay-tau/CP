9f3bf691cd159a93ca624f565ba27880f2ee95e8
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:13:27 +0530

    Fix typo in A_Problem-_1.py
    The problem statement says that the answer is the number of ways to choose
    a subset of the 1s such that the sum of the indices of the chosen 1s is
    divisible by 2^k.
    
    The code was computing the number of ways to choose a subset of the 1s
    such that the sum of the indices of the chosen 1s is divisible by 2^k
    times the number of 1s.

--- Codeforces/gym/395839/A_Problem-_1.py
+++ Codeforces/gym/395839/A_Problem-_1.py
@@ -1,9 +1,9 @@
 for _ in range(int(input())):
-	n = int(input())
-	a = list(map(int, input().split()))
-	zero_count = a.count(0)
-	one_count = a.count(1)
-	if zero_count == 0:
-		print(one_count)
-	else:
-		print((2**zero_count)*(one_count))
+    n = int(input())
+    a = list(map(int, input().split()))
+    zero_count = a.count(0)
+    one_count = a.count(1)
+    if zero_count == 0:
+        print(one_count)
+    else:
+        print((2**zero_count) * (one_count))
