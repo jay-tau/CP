b7697c80d97765d89983e31cfe9efac7b86d7ebc
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:17:00 +0530

    # Write a commit message describing the changes and the reasoning behind them git commit -F- <<

--- Codeforces/problemset/4/A_Watermelon.cpp
+++ Codeforces/problemset/4/A_Watermelon.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 
-
 int main() {
-	int w{};
-    std::cin >> w;
-    if (w == 2) {
-        std::cout << "NO";
-    } else if (w % 2 == 0) {
-        std::cout << "YES";
-    } else {
-        std::cout << "NO";
-    }
-    std::cout << '\n';
-	return 0;
+  int w{};
+  std::cin >> w;
+  if (w == 2) {
+    std::cout << "NO";
+  } else if (w % 2 == 0) {
+    std::cout << "YES";
+  } else {
+    std::cout << "NO";
+  }
+  std::cout << '\n';
+  return 0;
 }
--- Codeforces/problemset/4/C_Registration_system.py
+++ Codeforces/problemset/4/C_Registration_system.py
@@ -1,9 +1,9 @@
 d = {}
 for _ in range(int(input())):
-	s = input()
-	if s not in d:
-		d[s] = 0
-		print("OK")
-	else:
-		d[s] += 1
-		print(s + str(d[s]))
+    s = input()
+    if s not in d:
+        d[s] = 0
+        print("OK")
+    else:
+        d[s] += 1
+        print(s + str(d[s]))
