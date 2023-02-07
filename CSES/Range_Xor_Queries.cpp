88a024573776fa874193119b69b1e1f45ccded94
Joel Tony <joeltony72@gmail.com>
2023-02-07 19:35:14 +0530

    Add solution to CSES Forest Queries
    The solution is a simple prefix sum problem.

--- CSES/Range_Xor_Queries.cpp
+++ CSES/Range_Xor_Queries.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main() {
-    int n, q;
-    cin >> n >> q;
-    long long int x[n];
-    for (int i = 0; i < n; i++) {
-        int x_i;
-        cin >> x_i;
-        if (i == 0)
-            x[i] = x_i;
-        else
-            x[i] = x[i - 1]^x_i;
-    }
-    // for (auto i: x)
-        // cout << i << " ";
-    for (int i = 0; i < q; i++) {
-        int a, b;
-        cin >> a >> b;
-        int ans = (x[b-1])^((a>=2)?(x[a-2]):0);
-        cout << ans << '\n';
-    }
-    return 0;
+  int n, q;
+  cin >> n >> q;
+  long long int x[n];
+  for (int i = 0; i < n; i++) {
+    int x_i;
+    cin >> x_i;
+    if (i == 0)
+      x[i] = x_i;
+    else
+      x[i] = x[i - 1] ^ x_i;
+  }
+  // for (auto i: x)
+  // cout << i << " ";
+  for (int i = 0; i < q; i++) {
+    int a, b;
+    cin >> a >> b;
+    int ans = (x[b - 1]) ^ ((a >= 2) ? (x[a - 2]) : 0);
+    cout << ans << '\n';
+  }
+  return 0;
 }
