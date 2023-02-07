ce8ec40c7d6c47f8dd821ddedcf598514e593951
Joel Tony <joeltony72@gmail.com>
2023-02-07 19:04:46 +0530

    Add solutions for Codeforces problemset 1582 and 1584
    - Problem 1582: Luntik and Subsequences
    - Problem 1584: Two Arrays

--- Codeforces/problemset/1584/C_Two_Arrays.cpp
+++ Codeforces/problemset/1584/C_Two_Arrays.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-        sort(a, a+n);
-        int b[n];
-        for (int i = 0; i < n; i++){
-            cin >> b[i];
-        }
-        sort(b, b+n);
-        bool possible = true;
-        // bool added = false;
-        for (int i = 0; i < n; i++) {
-            if (!((a[i] == b[i]) || (b[i] - a[i] == 1))) {
-                possible = false;
-                break;
-            }
-        }
-        if (possible) {
-            cout << "YES" << '\n';
-        } else {
-            cout << "NO" << '\n';
-        }
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
     }
+    sort(a, a + n);
+    int b[n];
+    for (int i = 0; i < n; i++) {
+      cin >> b[i];
+    }
+    sort(b, b + n);
+    bool possible = true;
+    // bool added = false;
+    for (int i = 0; i < n; i++) {
+      if (!((a[i] == b[i]) || (b[i] - a[i] == 1))) {
+        possible = false;
+        break;
+      }
+    }
+    if (possible) {
+      cout << "YES" << '\n';
+    } else {
+      cout << "NO" << '\n';
+    }
+  }
 }
