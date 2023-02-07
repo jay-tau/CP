d89c91073919f460134882f9a41c451b8026bece
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:07:47 +0530

    Fix typos
    - Fix typos
    - Add a newline at the end of the file

--- Codeforces/gym/395258/A_Problem_1.cpp
+++ Codeforces/gym/395258/A_Problem_1.cpp
@@ -1,13 +1,13 @@
 /**
  *    author:  jaytau
-**/
+ **/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vint;
-typedef pair<int,int> intpair;
+typedef pair<int, int> intpair;
 
 #define endl "\n"
 #define YES cout << "YES" << endl
@@ -19,46 +19,41 @@ typedef pair<int,int> intpair;
 #define pb push_back
 #define mp make_pair
 
-#define fo(i, n) for (int (i) = 0; (i) < (n); (i)++)
+#define fo(i, n) for (int(i) = 0; (i) < (n); (i)++)
 
-#define fastio()                  \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);                   \
-	cout.tie(0)
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0)
 
-#define test() \
-	int _;     \
-	cin >> _;  \
-	while (_--)
+#define test()                                                                 \
+  int _;                                                                       \
+  cin >> _;                                                                    \
+  while (_--)
 
-#define test1() \
-	int t;      \
-	t = 1;      \
-	while (t--)
-
+#define test1()                                                                \
+  int t;                                                                       \
+  t = 1;                                                                       \
+  while (t--)
 
 void solve() {
-	int n;
-	cin >> n;
-	vint a(n);
-	fo(i, n) {
-		cin >> a[i];
-	}
-	int ans = 0;
-	while (!is_sorted(a.begin(), a.end())) {
-		for (int i = ans%2; (i+1) < n; i+=2) {
-			if (a[i] > a[i+1])
-				swap(a[i], a[i + 1]);
-		}
-		ans++;
-	}
-	cout << ans << endl;
+  int n;
+  cin >> n;
+  vint a(n);
+  fo(i, n) { cin >> a[i]; }
+  int ans = 0;
+  while (!is_sorted(a.begin(), a.end())) {
+    for (int i = ans % 2; (i + 1) < n; i += 2) {
+      if (a[i] > a[i + 1])
+        swap(a[i], a[i + 1]);
+    }
+    ans++;
+  }
+  cout << ans << endl;
 }
 
 int main() {
-	fastio();
-	test() {
-		solve();
-	}
-	return 0;
+  fastio();
+  test() { solve(); }
+  return 0;
 }
