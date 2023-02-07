05f53b90cf0927cf55f26f70a1dedf1bd951b2f5
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:21:00 +0530

    Add a newline at the end of the file
    This is a test commit message.

--- Codeforces/problemset/71/A_Way_Too_Long_Words.cpp
+++ Codeforces/problemset/71/A_Way_Too_Long_Words.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    int t{};
-    cin >> t;
-    while (t--) {
-        string s{};
-        cin >> s;
-        int len = s.length();
-        if (len > 10) {
-            cout << s[0] << (len-2) << s[len-1] << '\n';
-        } else {
-            cout << s << '\n';
-        }
+  int t{};
+  cin >> t;
+  while (t--) {
+    string s{};
+    cin >> s;
+    int len = s.length();
+    if (len > 10) {
+      cout << s[0] << (len - 2) << s[len - 1] << '\n';
+    } else {
+      cout << s << '\n';
     }
-    return 0;
+  }
+  return 0;
 }
