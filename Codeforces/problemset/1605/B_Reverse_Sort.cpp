31e20c284e9bd9849ed749596d3917483b3d8b78
Joel Tony <joeltony72@gmail.com>
2023-02-07 19:04:59 +0530

    Fix indentation
    The indentation was not consistent.

--- Codeforces/problemset/1605/B_Reverse_Sort.cpp
+++ Codeforces/problemset/1605/B_Reverse_Sort.cpp
@@ -2,43 +2,43 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
-        string sorted_s = s;
-        sort(sorted_s.begin(), sorted_s.end());
-        int count_0 = 0, count_1 = 1;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '0')
-                count_0++;
-            else
-                count_1++;
-        }
-        int flag = ((sorted_s == s) ? 0 : 1);
-        cout << flag << '\n';
-        if (flag) {
-            int k = 0;
-            vector<int> answer_indices;
-            for (int i = 0; i < n; i++) {
-                char current_char = s[i];
-                if ((i >= (count_0 - 1)) && current_char == '0') {
-                    answer_indices.push_back(i+1);
-                    k++;
-                } else if ((i <= (count_0 -1)) && current_char == '1') {
-                    answer_indices.push_back(i+1);
-                    k++;
-                }
-            }
-            cout << k << ' ';
-            for (int i = 0; i < k; i++) {
-                cout << answer_indices[i] << ' ';
-            }
-            cout << '\n';
-        }
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string sorted_s = s;
+    sort(sorted_s.begin(), sorted_s.end());
+    int count_0 = 0, count_1 = 1;
+    for (int i = 0; i < n; i++) {
+      if (s[i] == '0')
+        count_0++;
+      else
+        count_1++;
     }
-    return 0;
+    int flag = ((sorted_s == s) ? 0 : 1);
+    cout << flag << '\n';
+    if (flag) {
+      int k = 0;
+      vector<int> answer_indices;
+      for (int i = 0; i < n; i++) {
+        char current_char = s[i];
+        if ((i >= (count_0 - 1)) && current_char == '0') {
+          answer_indices.push_back(i + 1);
+          k++;
+        } else if ((i <= (count_0 - 1)) && current_char == '1') {
+          answer_indices.push_back(i + 1);
+          k++;
+        }
+      }
+      cout << k << ' ';
+      for (int i = 0; i < k; i++) {
+        cout << answer_indices[i] << ' ';
+      }
+      cout << '\n';
+    }
+  }
+  return 0;
 }
