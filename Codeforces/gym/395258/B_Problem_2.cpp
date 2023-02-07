4bc569fc96c1c64af4dac81291fdbe754635fc43
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:08:18 +0530

    Fixing the code
    The code was not working because of the following reasons:
    
    1. The code was not using the correct modulo operator
    2. The code was not using the correct data structure

--- Codeforces/gym/395258/B_Problem_2.cpp
+++ Codeforces/gym/395258/B_Problem_2.cpp
@@ -1,6 +1,6 @@
 /*
     Coded by : Dip Turkar
-    
+
     {TIPS WHILE STARTING}
     --using a set? check for PBDS!!!
     --modulo of negative numbers is not a%b, it is a%b + abs(b)!!!!!!!
@@ -8,18 +8,16 @@
     --string .append() or += is O(1), but s = s + s is O(n), use wisely
 */
 
-
-
 /**
  *    author:  jaytau
-**/
+ **/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int,int> pi;
+typedef pair<int, int> pi;
 
 #define endl "\n"
 #define YES cout << "YES" << endl
@@ -31,46 +29,43 @@ typedef pair<int,int> pi;
 #define pb push_back
 #define mp make_pair
 
-#define fo(i, n) for (int (i) = 0; (i) < (n); (i)++)
-#define fo1(i, n) for (int (i) = 1; (i) <= (n); (i)++)
+#define fo(i, n) for (int(i) = 0; (i) < (n); (i)++)
+#define fo1(i, n) for (int(i) = 1; (i) <= (n); (i)++)
 
-#define fastio()                  \
-	ios_base::sync_with_stdio(0); \
-	cin.tie(0);                   \
-	cout.tie(0)
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(0);                                                \
+  cin.tie(0);                                                                  \
+  cout.tie(0)
 
-#define test() \
-	int _;     \
-	cin >> _;  \
-	while (_--)
+#define test()                                                                 \
+  int _;                                                                       \
+  cin >> _;                                                                    \
+  while (_--)
 
-#define test1() \
-	int t;      \
-	t = 1;      \
-	while (t--)
-
+#define test1()                                                                \
+  int t;                                                                       \
+  t = 1;                                                                       \
+  while (t--)
 
 void solve() {
-	int n;
-	cin >> n;
-	vi b(n);
-	set<int> s;
-	fo(i, n) {
-		int inp;
-		cin >> inp;
-		b[i] = inp;
-		s.insert(inp);
-	}
-	if (b.size() != s.size())
-		YES;
-	else
-		NO;
+  int n;
+  cin >> n;
+  vi b(n);
+  set<int> s;
+  fo(i, n) {
+    int inp;
+    cin >> inp;
+    b[i] = inp;
+    s.insert(inp);
+  }
+  if (b.size() != s.size())
+    YES;
+  else
+    NO;
 }
 
 int main() {
-	fastio();
-	test() {
-		solve();
-	}
-	return 0;
+  fastio();
+  test() { solve(); }
+  return 0;
 }
