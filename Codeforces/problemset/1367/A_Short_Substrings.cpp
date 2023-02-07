2cf7f7a465898af4f8971be28354fbcf268936fd
Joel Tony <joeltony72@gmail.com>
2023-02-07 11:40:13 +0530

    Add a newline at the end of the file
    This is a good practice to follow.

--- Codeforces/problemset/1367/A_Short_Substrings.cpp
+++ Codeforces/problemset/1367/A_Short_Substrings.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    int t{};
-    cin >> t;
-    while (t--) {
-        string s{};
-        cin >> s;
-        string ans{s[0]};
-        int len = s.length();
-        for (int i = 1; i < len; i++) {
-            if (i % 2 != 0) {
-                ans += s[i];
-            }
-        }
-        cout << ans << '\n';
+  int t{};
+  cin >> t;
+  while (t--) {
+    string s{};
+    cin >> s;
+    string ans{s[0]};
+    int len = s.length();
+    for (int i = 1; i < len; i++) {
+      if (i % 2 != 0) {
+        ans += s[i];
+      }
     }
-    return 0;
+    cout << ans << '\n';
+  }
+  return 0;
 }
