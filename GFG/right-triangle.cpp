173f2aa7ffba7f09c8ff8b6e37a5d4b294d513f0
Joel Tony <joeltony72@gmail.com>
2023-05-22 21:45:30 +0530

    Add patterns

---
+++ GFG/right-triangle.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+  void printTriangle(int n) {
+    for (int i = 1; i <= n; ++i) {
+      for (int j = 0; j < i; ++j)
+        cout << "* ";
+      cout << endl;
+    }
+  }
+};
---
+++ GFG/triangle-pattern.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+  void printTriangle(int n) {
+    for (int i = n; i >= 1; --i) {
+      for (int j = 0; j < i; ++j)
+        cout << "* ";
+      cout << endl;
+    }
+  }
+};
