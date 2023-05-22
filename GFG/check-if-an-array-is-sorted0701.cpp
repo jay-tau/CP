c094b73956b1ab25effe74e5bbc6b8973b07f639
Joel Tony <joeltony72@gmail.com>
2023-05-22 22:15:08 +0530

    Arrays easy problems

---
+++ GFG/check-if-an-array-is-sorted0701.cpp
@@ -0,0 +1,12 @@
+// User function template for C++
+
+class Solution {
+public:
+  bool arraySortedOrNot(int arr[], int n) {
+    for (int i = 1; i < n; ++i) {
+      if (!(arr[i - 1] <= arr[i]))
+        return false;
+    }
+    return true;
+  }
+};
---
+++ GFG/largest-element-in-array4009.cpp
@@ -0,0 +1,11 @@
+// User function Template for C++
+
+class Solution {
+public:
+  int largest(vector<int> &arr, int n) {
+    int largest = arr[0];
+    for (int i = 1; i < n; ++i)
+      largest = max(largest, arr[i]);
+    return largest;
+  }
+};
