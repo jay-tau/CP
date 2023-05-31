f230045cc6011e6cc88437bc00aacf7a7ec3f110
Joel Tony <joeltony72@gmail.com>
2023-05-31 20:24:30 +0530

    Add problem

---
+++ GFG/palindrome0746.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+class Solution {
+public:
+  string is_palindrome(int n) {
+    int n_rev = 0, n_copy = n;
+    while (n > 0) {
+      n_rev *= 10;
+      n_rev += (n % 10);
+      n /= 10;
+    }
+    return (n_rev == n_copy) ? "Yes" : "No";
+  }
+};
