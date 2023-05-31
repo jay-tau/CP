a91cc22900fc969dc0f56573583da8670e2c92ae
Joel Tony <joeltony72@gmail.com>
2023-05-31 23:19:30 +0530

    Add problem

---
+++ GFG/sum-of-all-divisors-from-1-to-n4738.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+class Solution {
+public:
+  long long sumOfDivisors(int N) {
+    long long ans = 0;
+    for (int i = 1; i <= N; ++i)
+      ans += ((N / i) * i);
+    return ans;
+  }
+};
