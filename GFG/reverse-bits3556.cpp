a198405f6b7a81a065fda4fd8c158320d6eaf50a
Joel Tony <joeltony72@gmail.com>
2023-05-31 19:53:21 +0530

    Update solution

--- GFG/reverse-bits3556.cpp
+++ GFG/reverse-bits3556.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
   long long reversedBits(long long X) {
     long long ans = 0;
-    int num_bits = 32;
-    while (num_bits--) {
-      // int bit = X % 2;
-      ans <<= 1;
-      ans += X % 2;
-      X >>= 1;
-      // ans = (2*ans) + bit;
+    int n = 32;
+    while (n--) {
+      ans *= 2;
+      ans += (X % 2);
+      X /= 2;
     }
     return ans;
   }
