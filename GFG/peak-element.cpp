1fac7077f8ddd4fb409cfa2359654b2bc2923843
Joel Tony <joeltony72@gmail.com>
2023-06-01 14:39:01 +0530

    Optimal solution for peak element

--- GFG/peak-element.cpp
+++ GFG/peak-element.cpp
@@ -5,18 +5,29 @@
 // n: size of array
 class Solution {
 public:
-  int peakElement(int arr[], int n) { // Single element
+  int peakElement(int arr[], int n) {
     if (n == 1)
       return 0;
-    // First element
-    if (arr[0] >= arr[1])
-      return 0;
-    // Last element
-    if (arr[n - 1] >= arr[n - 2])
-      return n - 1;
-    for (int i = 1; i < (n - 1); ++i) {
-      if ((arr[i - 1] <= arr[i]) && (arr[i] >= arr[i + 1]))
-        return i;
+    if (n == 2)
+      return (arr[0] >= arr[1]) ? 0 : 1;
+
+    int l = 0, r = n - 1;
+    while (l <= r) {
+      int mid = (l + r) / 2;
+
+      int current_element = arr[mid];
+      int left_element = ((mid - 1) < 0) ? INT_MIN : arr[mid - 1];
+      int right_element = ((mid + 1) >= n) ? INT_MIN : arr[mid + 1];
+
+      if ((left_element <= current_element) &&
+          (current_element >= right_element)) // Peak
+        return mid;
+      if ((left_element <= current_element) &&
+          (current_element <= right_element)) // Increasing
+        l = mid + 1;                          // Move right
+      else                                    // Decreasing
+        r = mid - 1;
     }
+    return -1; // No possible answer
   }
 };
