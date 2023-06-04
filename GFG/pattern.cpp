dd97d969305e44d1a13195c2b0407e49ecc06a9d
Joel Tony <joeltony72@gmail.com>
2023-06-04 08:36:01 +0530

    Add star patterns

---
+++ GFG/pattern.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  void printDiamond(int n) {
+    for (int i = 1; i <= n; ++i) {
+      int num_spaces = (n - i);
+
+      for (int j = 0; j < num_spaces; ++j)
+        cout << " ";
+      for (int j = 0; j < i; ++j)
+        cout << "* ";
+      cout << endl;
+    }
+    for (int i = n; i >= 1; --i) {
+      int num_spaces = (n - i);
+
+      for (int j = 0; j < num_spaces; ++j)
+        cout << " ";
+      for (int j = 0; j < i; ++j)
+        cout << "* ";
+      cout << endl;
+    }
+  }
+};
