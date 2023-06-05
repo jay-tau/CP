cad71f06a494298c94413a3f229642a0f1896c73
Joel Tony <joeltony72@gmail.com>
2023-06-05 16:35:45 +0530

    Add recursion problem

---
+++ GFG/power-of-numbers-1587115620.cpp
@@ -0,0 +1,15 @@
+class Solution {
+ public:
+  // You need to complete this fucntion
+
+  long long power(int N, int R) {
+    const long long m = 1e9 + 7;
+
+    if (R == 0) return 1;
+
+    if (R % 2 == 0)
+      return power(((N % m) * (N % m)) % m, (R / 2));
+    else
+      return ((N % m) * (power(N, R - 1) % m)) % m;
+  }
+};
