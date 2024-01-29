cc630d886b85f5dca8175c53a78a98811ea22806
Joel Tony <joeltony72@gmail.com>
2024-01-29 22:54:47 +0530

    Update top-view-of-binary-tree

--- GFG/top-view-of-binary-tree.cpp
+++ GFG/top-view-of-binary-tree.cpp
@@ -6,17 +6,15 @@ struct Node
     Node* right;
 };
 */
+
 class Solution {
  public:
   // Function to return a list of nodes visible from the top view
   // from left to right in Binary Tree.
   vector<int> topView(Node* root) {
-    if (root == nullptr) return {};
-
-    map<int, int> m;            // { offset, node}
-    queue<pair<Node*, int>> q;  // {node, offset}
-
-    q.push({root, 0});
+    map<int, int> m;            // (offset, node_value)
+    queue<pair<Node*, int>> q;  // (node, offset)
+    if (root) q.push({root, 0});
 
     while (!q.empty()) {
       pair<Node*, int> p = q.front();
@@ -24,20 +22,17 @@ class Solution {
       Node* current_node = p.first;
       int current_offset = p.second;
 
-      if (current_node == nullptr) continue;
-
-      if (m.find(current_offset) == m.end())  // Element not present
+      if (m.find(current_offset) == m.end())  // Offset not present
         m[current_offset] = current_node->data;
 
-      // Insert its children
-      q.push({current_node->left, current_offset - 1});
-      q.push({current_node->right, current_offset + 1});
+      if (current_node->left) q.push({current_node->left, current_offset - 1});
+      if (current_node->right)
+        q.push({current_node->right, current_offset + 1});
     }
 
     vector<int> top_view;
-    for (pair<int, int> p : m) {
-      int offset = p.first, node = p.second;
-      top_view.push_back(node);
+    for (auto p : m) {
+      top_view.push_back(p.second);
     }
 
     return top_view;
